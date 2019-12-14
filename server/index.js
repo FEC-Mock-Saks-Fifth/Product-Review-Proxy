bf6f58b6119241d23489520040e0916efe977c26
Aria Keshmiri <ariakesh@yahoo.com>
2019-12-14 15:19:33 -0800

    completes proxy server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const router = require('./router.js')
-const bodyParser = require('body-parser')
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+const { routes } = require('../reverseproxyconfig.json');
+const cors = require('cors');
 
-app.use(cors())
+const app = express();
 
-app.use(bodyParser.json())
+/* SERVE PRODUCT REVIEWS STATIC FILES -- this needs to serve at this location in order for content to populate & have post route function */
+app.use('/reviews', express.static(path.resolve(__dirname, '../../Product-Review/client/dist/')));
 
-app.use(bodyParser.urlencoded({extended:true}))
+/* ROUTE DELEGATION TO EXTERNAL MODULES - refer to reverseproxyconfig.json for detailed routes */
+/* moved this above bodyparser for post route to work */
+for (route of routes) {
+  app.use(route.route,
+      proxy({
+          target: route.address,
+          pathRewrite: (path, req) => {
+            // console.log("USING PROXY ROUTER", `path: ${path}`, `route.address: ${route.address}`);
+            return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+          }
+      })
+  );
+}
 
-app.use('/api',router)
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("/Users/ariakesh/Product-Review/client/dist"))
+/* STATIC FILE SERVES */
+/* static html & css files in THIS proxy server */
+app.use(express.static(path.resolve(__dirname, '../common')));
 
-module.exports = app
+/* module bundle paths */
+app.use('/recs', express.static(path.resolve(__dirname, '../../Related-Recently-Viewed-Products/client/public/')));
+app.use('/search', express.static(path.resolve(__dirname ,'../../Nav-Search/client/dist/')));
+app.use('/productdetail', express.static(path.resolve(__dirname, '../../Product-Description/client/dist/')));
+
+var port = 3013;
+app.listen(port, () => {console.log(`app is listening on port: ${port}`)});
+
+module.exports = app;
--- server/start.js
+++
@@ -1,4 +0,0 @@
-const app = require('./index.js')
-const port = 3000
-
-app.listen(port, () => console.log(`HIS POWER LEVELS ARE OVER ${port}000000000!`))
